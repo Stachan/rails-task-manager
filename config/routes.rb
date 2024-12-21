649dacc02202ddbad5fe192018b66ac09fb0a93e
Stamati Stamatiou <stachan66@gmail.com>
2024-12-21 16:36:52 +0900

    Finished Models&CRUD 2-Rails Task Manager; not pretty

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # INDEX
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  # NEW/CREATE
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+  # SHOW
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # EDIT/UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy"
 end
